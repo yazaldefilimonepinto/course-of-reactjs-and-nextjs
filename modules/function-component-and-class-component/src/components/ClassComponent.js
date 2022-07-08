eb026b0fe8b9f7cc051632d5e6a3bb62f4e9b18f
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-08 20:22:47 +0200

    feat: lerning class and function component

---
+++ modules/function-component-and-class-component/src/components/ClassComponent.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class ClassComponent extends Component {
+  render() {
+    return <h1>Hello ClassComponent</h1>;
+  }
+}
---
+++ modules/function-component-and-class-component/src/components/FunctionComponent.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FunctionComponent = () => {
+  return <h1>hello FunctionComponent</h1>;
+};
